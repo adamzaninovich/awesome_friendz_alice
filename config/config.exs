489a5d58b07f9a647c379fe43a071eb39cf68e3c
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2016-02-14 04:29:48 -0600

    update config for google images

--- config/config.exs
+++ config/config.exs
@@ -1,8 +1,8 @@
 use Mix.Config
 
-config :alice, :google_images_cse_id, System.get_env("GOOGLE_CSE_ID")
-config :alice, :google_images_cse_token, System.get_env("GOOGLE_CSE_TOKEN")
-config :alice, :google_images_safe_search_level, :medium
+config :alice_google_images, :cse_id, System.get_env("GOOGLE_CSE_ID")
+config :alice_google_images, :cse_token, System.get_env("GOOGLE_CSE_TOKEN")
+config :alice_google_images, :safe_search_level, :medium
 
 case Mix.env do
   env when env in [:prod, :dev] -> import_config "#{env}.exs"
