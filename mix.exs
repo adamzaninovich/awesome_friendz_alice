c90fd2138417175ce7a621e70c183c209ee7e23a
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2016-02-14 04:30:02 -0600

    updates deps to point to new hex packages

--- mix.exs
+++ mix.exs
@@ -24,9 +24,9 @@ defmodule AwesomeFriendzAlice.Mixfile do
   defp deps do
      [
        {:websocket_client, github: "jeremyong/websocket_client"},
-       {:alice, "~> 0.1.0"},
-       {:alice_against_humanity, github: "adamzaninovich/alice_against_humanity"},
-       {:alice_google_images, github: "adamzaninovich/alice_google_images"}
+       {:alice,                  "~> 0.1.0"},
+       {:alice_against_humanity, "~> 0.0.1"},
+       {:alice_google_images,    "~> 0.0.1"}
      ]
   end
 end
