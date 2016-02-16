bcfe10c3b4e3da1f78ac3879f51fcf949b516bb8
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2016-02-15 23:17:36 -0600

    adds Alice Karma, updates Alice

--- mix.exs
+++ mix.exs
@@ -2,31 +2,33 @@ defmodule AwesomeFriendzAlice.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :awesome_friendz_alice,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [ app: :awesome_friendz_alice,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps ]
   end
 
   def application do
-    [applications: [:alice],
+    [ applications: [:alice],
       mod: {
         Alice, [
           Alice.Handlers.Random,
           Alice.Handlers.OhYouSo,
           Alice.Handlers.AgainstHumanity,
-          Alice.Handlers.GoogleImages
+          Alice.Handlers.GoogleImages,
+          Alice.Handlers.Karma
         ] } ]
   end
 
   defp deps do
      [
        {:websocket_client, github: "jeremyong/websocket_client"},
-       {:alice,                  "~> 0.1.0"},
+       {:alice,                  "~> 0.1.1"},
        {:alice_against_humanity, "~> 0.0.1"},
-       {:alice_google_images,    "~> 0.0.1"}
+       {:alice_google_images,    "~> 0.0.1"},
+       {:alice_karma,            "~> 0.0.1"}
      ]
   end
 end
