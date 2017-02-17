a2360478c1dde791da83d7ab417127c0b02a2ac6
Tyler Clemens <tyler.r.clemens@gmail.com>
2017-02-17 12:47:09 -0800

    Maybe now fixed, plx? I need more positive karma. KThxBye

--- lib/alice/handlers/random.ex
+++ lib/alice/handlers/random.ex
@@ -41,7 +41,7 @@ defmodule Alice.Handlers.Random do
   command ~r/\beggplant me\z/i,                            :single_eggplant
   command ~r/\beggplant me (\d+)\z/i,                      :multi_eggplant
   command ~r/\ba?re? (yo)?u high\??/i,                     :are_you_high
-  route ~r/(?<!img me \b)bass\b/i,                         :bass_face
+  route ~r/(?<!img me este haim )bass/i,                   :bass_face
 
   @doc false
   def flowery_bullshit(conn), do: "http://i.imgur.com/ioLp3DW.jpg" |> reply(conn)
