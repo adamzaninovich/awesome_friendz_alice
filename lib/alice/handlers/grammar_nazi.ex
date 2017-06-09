7d0ce446854d06f567925358a4f92018e7a623bf
Adam Zaninovich <adam.zaninovich@gmail.com>
2017-06-09 10:48:36 -0700

    fixes broke shit

--- lib/alice/handlers/grammar_nazi.ex
+++ lib/alice/handlers/grammar_nazi.ex
@@ -7,8 +7,8 @@ defmodule Alice.Handlers.GrammarNazi do
   route ~r/\b([wcs]ould(n't)?) of\b/i, :correct_of
 
   @doc "Corrects your terrible grammar. Not an actual Nazi."
-  def correct_of(conn) do
-    word = Conn.first_capture(conn)
+  def correct_of(%Conn{message: %{captures: captures}}=conn) do
+    [_full, word | _rest] = captures
     delayed_reply(~s(or maybe "#{word} have"), 1200, conn)
     delayed_reply("Anyway, you're welcome! :+1:", 3600, conn)
     reply(conn, ~s(Hey #{Conn.at_reply_user(conn)}, I think you mean "#{word}'ve"))
