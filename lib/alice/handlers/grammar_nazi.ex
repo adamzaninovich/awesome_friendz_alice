ab0f6b48d2a45ee69cb3b6e2ae854b2bb36115b1
Adam Zaninovich <adam.zaninovich@gmail.com>
2017-06-09 10:34:51 -0700

    support n't as well

--- lib/alice/handlers/grammar_nazi.ex
+++ lib/alice/handlers/grammar_nazi.ex
@@ -4,21 +4,11 @@ defmodule Alice.Handlers.GrammarNazi do
 
   alias Alice.Conn
 
-  route ~r/\bwould of\b/i, :would_of
-  route ~r/\bcould of\b/i, :could_of
-  route ~r/\bshould of\b/i, :should_of
+  route ~r/\b([wcs]ould(n't)?) of\b/i, :correct_of
 
-  @doc false
-  def would_of(conn), do: correct_of(conn, "would")
-
-  @doc false
-  def could_of(conn), do: correct_of(conn, "could")
-
-  @doc false
-  def should_of(conn), do: correct_of(conn, "should")
-
-  @doc false
-  def correct_of(conn, word) do
+  @doc "Corrects your terrible grammar. Not an actual Nazi."
+  def correct_of(conn) do
+    word = Conn.first_capture(conn)
     delayed_reply(~s(or maybe "#{word} have"), 1200, conn)
     delayed_reply("Anyway, you're welcome! :+1:", 3600, conn)
     reply(conn, ~s(Hey #{Conn.at_reply_user(conn)}, I think you mean "#{word}'ve"))
