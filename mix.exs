2653ea503d44fde942a3aaccc4b3f1cd318ef7f2
Adam Zaninovich <adam.zaninovich@gmail.com>
2017-08-29 10:26:48 -0500

    update deps

--- mix.exs
+++ mix.exs
@@ -26,7 +26,6 @@ defmodule AwesomeFriendzAlice.Mixfile do
       Alice.Handlers.GoogleImages,
       Alice.Handlers.Karma,
       Alice.Handlers.Shizzle,
-      Alice.Handlers.Xkcd,
       Alice.Handlers.Reddit,
       Alice.Handlers.Dogeme,
       Alice.Handlers.Eats
@@ -36,15 +35,13 @@ defmodule AwesomeFriendzAlice.Mixfile do
   defp deps do
      [
        {:alice, github: "alice-bot/alice", branch: "master", override: true},
-       {:httpoison,                "0.11.0", override: true},
        {:alice_against_humanity,   "~> 0.1"},
        {:alice_google_images,      "~> 0.1"},
        {:alice_karma,              "~> 0.3"},
        {:alice_shizzle,            "~> 0.1"},
-       {:alice_xkcd,               "~> 0.0"},
-       {:alice_reddit,             "~> 0.0.4"},
-       {:alice_doge_me,            "~> 0.1.0"},
-       {:alice_eats,               "~> 0.1.0"}
+       {:alice_reddit,             "~> 0.1"},
+       {:alice_doge_me,            "~> 0.1"},
+       {:alice_eats,               "~> 0.1"}
      ]
   end
 end
