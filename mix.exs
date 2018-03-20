75e150e5771491b16ca065f3224b6b5ea590c8b9
Adam Zaninovich <adam.zaninovich@gmail.com>
2018-03-19 23:43:08 -0700

    removes wiki

--- mix.exs
+++ mix.exs
@@ -29,8 +29,7 @@ defmodule AwesomeFriendzAlice.Mixfile do
       Alice.Handlers.Shizzle,
       Alice.Handlers.Reddit,
       Alice.Handlers.Dogeme,
-      Alice.Handlers.Eats,
-      Alice.Handlers.AliceWiki
+      Alice.Handlers.Eats
     ]
   end
 
@@ -44,8 +43,8 @@ defmodule AwesomeFriendzAlice.Mixfile do
        {:alice_shizzle,          "~> 0.1"},
        {:alice_reddit,           "~> 0.1"},
        {:alice_doge_me,          "~> 0.1"},
-       {:alice_eats,             "~> 0.1"},
-       {:alice_wiki,             "~> 1.0"}
+       {:alice_eats,             "~> 0.1"}
+       # {:alice_wiki,             "~> 1.0"}
      ]
   end
 end
