92844df19786e2a9be651427e5872811938f1e52
Tyler Clemens <tyler.r.clemens@gmail.com>
2018-03-28 11:35:36 -0700

    Add alice_weather

--- config/config.exs
+++ config/config.exs
@@ -8,6 +8,15 @@ config :alice_google_images,
 config :awesome_friendz_alice,
   bitly_token: "c12043a1cedb06f76a46d3765a41b4dfa41df3fb"
 
+config :alice_weather,
+  api_key: System.get_env("DARKSKY_API_KEY")
+
+config :geocodex,
+  api_key: System.get_env("GOOGLE_GEOCODING_API_KEY")
+
+config :geocodex,
+  geocode_api_url: "https://maps.googleapis.com/maps/api/geocode"
+
 case Mix.env do
   env when env in [:prod, :dev] -> import_config "#{env}.exs"
   _other                        -> import_config "other.exs"
