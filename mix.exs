ab2dba3aa390fbd6c343614e51e2e57b8fd50989
Adam Zaninovich <adam.zaninovich@gmail.com>
2018-03-30 20:23:11 -0700

    use elixir 1.5.x

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AwesomeFriendzAlice.Mixfile do
   def project do
     [ app: :awesome_friendz_alice,
       version: "0.2.3",
-      elixir: "~> 1.2",
+      elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps() ]
