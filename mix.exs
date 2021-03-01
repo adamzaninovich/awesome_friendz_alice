24ebcf0acd8e5e923ca148d19d6c92fa757f8692
Tyler Clemens <tyler.r.clemens@gmail.com>
2021-03-01 14:03:11 -0700

    bump alice to 2.0

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule AwesomeFriendzAlice.Mixfile do
       {:alice_eats, "~> 0.1"},
       {:alice_wiki, "~> 1.0"},
       {:alice_weather, github: "alice-bot/alice_weather", branch: "master"},
-      {:alice_haha, "~> 1.0"},
+      {:alice_haha, "~> 2.0"},
       # {:alice_good_morning, "~> 1.0"},
       {:alice_good_morning, github: "tielur/alice_good_morning", branch: "master"},
       {:alice_roller, "~> 0.3"}
